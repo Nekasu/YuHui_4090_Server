51bfbe881b473ce7879aeddb9be0332470525a0d
zxt_from_YuHui_4090_Server <zxt1428147954@163.com>
2024-05-23 10:40:27 +0800

    将使用metric3d模型的代码进行封装, 能够直接调用use_metric3d文件中的函数, 传入图像路径直接调用该模型

---
+++ 3_code_area/0_code_hello/test_metirc3d/use_metric3d.py
@@ -0,0 +1,42 @@
+import main
+import torch
+import cv2
+
+def use_metric3d(rgb_file_path):
+    """
+    一个用于直接调用metric3d模型的函数, 集成了图像读取, 相机内参定义, unpad以及逆标准相机变换的步骤.
+    
+    rgb_file_path参数：需要进行深度预测的文件路径.
+    
+    返回：深度预测的结果, 一个张量(Tensor).
+    """
+    origin_rgb_file_name = rgb_file_path.split(sep='/')[-1]
+    visible_file_path = '/mnt/sda/zxt/3_code_area/0_code_hello/test_metirc3d/enhanced_'+origin_rgb_file_name
+        # 相机内参
+    intrinsic = [707.0493, 707.0493, 604.0814, 180.5066]
+    gt_depth_scale = 256.0
+        # 读取图像
+    rgb_origin = cv2.imread(rgb_file_path)[:, :, ::-1]
+        # 调整输入图像大小以适应预训练模型
+    rgb, pad_info, intrinsic = main.adjust_input_size(image=rgb_origin, intrinsic=intrinsic)
+        # 归一化
+    rgb = main.normalize(rgb)
+
+    # model loading
+    model = torch.hub.load('yvanyin/metric3d', 'metric3d_vit_small', pretrain=True)
+    model.cuda().eval()
+
+    # model inference
+    with torch.no_grad():
+        pred_depth, confidence, output_dict = model.inference({'input': rgb})
+        
+
+    # post processing
+    pred_depth = main.unpad(pred_depth, rgb_origin, pad_info)
+    pred_depth = main.decanonical_transform(intrinsic=intrinsic, pred_depth=pred_depth)
+    
+    return pred_depth
+
+if __name__ == '__main__':
+    rgb_file_path = '/mnt/sda/Dataset/Detection/COCO/test2017/000000000001.jpg'
+    print(use_metric3d(rgb_file_path=rgb_file_path))
