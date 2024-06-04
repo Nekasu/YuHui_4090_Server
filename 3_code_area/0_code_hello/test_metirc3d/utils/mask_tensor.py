5bc8070d20b4f6c2987ab30cd62fc17755c30547
zxt_from_YuHui_4090_Server <zxt1428147954@163.com>
2024-06-04 17:03:45 +0800

    更改show_save_mask函数, 新增isshow参数

--- 3_code_area/0_code_hello/test_metirc3d/utils/mask_tensor.py
+++ 3_code_area/0_code_hello/test_metirc3d/utils/mask_tensor.py
@@ -119,7 +119,7 @@ def get_range_negative_mask_tensor(depth_tensor: torch.Tensor, list_t: List, mod
     else:
         print("wrong mode error")
 
-def show_save_mask(depth_tensor: torch.Tensor, save_path=None):
+def show_save_mask(mask_tensor: torch.Tensor, save_path=None, isshow=True):
     """
     一个用于可视化带有掩膜的深度检测张量(Tensor)的函数.
 
@@ -128,23 +128,25 @@ def show_save_mask(depth_tensor: torch.Tensor, save_path=None):
     t参数: 需要掩膜的数值, 例如, 如果t=4, 则将深度检测张量中数值不为4的分量全部改成0;
 
     path参数：保存掩膜图像的位置;
+    
+    isshow参数：控制该函数是否将掩膜可视化, True为显示掩膜, 默认为True
 
     返回：无返回值.
     """
-    depth_tensor = depth_tensor.cpu()
-    # 将数值不为4的分量全部改成0
-    masked_tensor = torch.where(depth_tensor == t, torch.tensor(255, dtype=torch.uint8), torch.tensor(0, dtype=torch.uint8))
-
+    if save_path is not None:
+        mask_tensor.save(save_path)
+    
+    mask_tensor = mask_tensor.cpu()
+    
     # 将 Tensor 转换为 NumPy 数组并转换为uint8类型
-    masked_array = masked_tensor.numpy().astype(np.uint8)
+    masked_array = mask_tensor.numpy().astype(np.uint8)
     print(masked_array)
 
-    # 使用 PIL.Image 显示图像
     masked_image = Image.fromarray(masked_array)  # 'L' 模式表示灰度图像
-    masked_image.show()
     
-    if save_path is not None:
-        masked_image.save(save_path)
+    if isshow:
+        # 使用 PIL.Image 显示图像
+        masked_image.show()
 
 if __name__ == "__main__":
     import use_metric3d
