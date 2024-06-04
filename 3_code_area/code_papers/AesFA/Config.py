e05712a4993d99795e3b2558c3e405c4100c86b1
zxt_from_YuHui_4090_Server <zxt1428147954@163.com>
2024-06-04 11:26:12 +0800

    获取cameraman的风格迁移结果

--- 3_code_area/code_papers/AesFA/Config.py
+++ 3_code_area/code_papers/AesFA/Config.py
@@ -21,7 +21,7 @@ class Config:
         test_style_size = 256
         content_dir = content_dir
         style_dir = style_dir
-        img_dir = './output/'+file_n+'/'+ 'cameraman' + '/' + 'backgound'
+        img_dir = './output/'+file_n+'/'+ 'cameraman' + '/' + 'background'
 
     elif phase == 'style_blending':
         blend_load_size = 256
